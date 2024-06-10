decce7fdd2c7060c995b099097f8f1b7fe6f45f0
KR-EGOIST <jhso5041@naver.com>
2024-06-10 11:54:57 +0900

    ✨ update : 커넥션 핸들러, 서버에서 유저를 위한 데이터를 생성해 저장

--- src/handlers/register.handler.js
+++ src/handlers/register.handler.js
@@ -9,7 +9,8 @@ socket.on 을 하면 하나의 유저를 대상으로 한 이벤트가 처리됩
 // uuid 를 생성하는 모듈 uuid , npm i uuid
 import { v4 as uuidv4 } from 'uuid';
 import { addUser } from '../models/user.model.js';
-import { handleDisconnect } from './helper.js';
+import { handleDisconnect, handleConnection } from './helper.js';
+import { createStage } from '../models/stage.model.js';
 
 const registerHandler = (io) => {
   // connection 이라는 이벤트가 발생되면 io.on 의 콜백함수가 실행된다.
@@ -22,6 +23,11 @@ const registerHandler = (io) => {
     const userUUID = uuidv4();
     // socket.id 인자로 들어오는 socket 이 id를 가지고 있습니다.
     addUser({ uuid: userUUID, socketId: socket.id });
+
+    createStage(uuerUUID);
+
+    // 접속시 유저 정보 생성 이벤트 처리
+    handleConnection(socket, userUUID);
 
     // 접속 해제시 이벤트 처리, 유저가 접속을 끊었을 경우
     socket.on('disconnect', () => {
