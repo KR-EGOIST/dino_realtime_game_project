8b63f611b244d3a27b26052bfd53370534dffd4f
KR-EGOIST <jhso5041@naver.com>
2024-06-12 16:37:48 +0900

    🐛 bugfix : redis url 수정

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import redis from 'redis';
 dotenv.config();
 
 const redisClient = redis.createClient({
-  url: `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
   legacyMode: true,
 });
 
